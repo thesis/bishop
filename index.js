064a501b9e831200192f54a0c574f1679f4ec5cd
Beau Shinkle <beaushinkle@gmail.com>
2022-03-17 13:23:26 -0400

    Update the intents

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const ROLE = process.env.ROLE
 const EMOJI = process.env.EMOJI
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new Client({ intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_MEMBERS,
+] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
