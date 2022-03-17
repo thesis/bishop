e40e118c86625beba4447433ee5328b06c432c9c
Beau Shinkle <beaushinkle@gmail.com>
2022-03-16 21:10:10 -0400

    We have a working robot

---
+++ index.js
@@ -0,0 +1,31 @@
+const TOKEN = process.env.TOKEN
+const ROLE = process.env.ROLE
+const EMOJI = process.env.EMOJI
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('threadCreate', async thread => {
+  thread.join()
+  thread.send("<placeholder").then(placeholder => {
+    placeholder.edit("<@&" + ROLE + ">")
+  })
+});
+
+client.on('messageCreate', async message => {
+  if (!!message.reference) {
+    client.channels.fetch(message.reference.channelId).then(channel => {
+      channel.messages.fetch(message.reference.messageId).then(referenceMessage => {
+        if (!!referenceMessage.reference) {
+          message.react(EMOJI)
+        }
+      })
+    })
+  }
+});
+
+client.login(TOKEN);
