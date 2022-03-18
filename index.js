299a3a7510c49348a06160731e454c93cefb073e
Beau Shinkle <beaushinkle@gmail.com>
2022-03-18 13:24:35 -0400

    Fix handling for standups

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ client.on('ready', async () => {
 });
 
 client.on('threadCreate', async thread => {
-  if (thread.parent.name !== "standups") {
+  const parent = await thread.guild.channels.fetch(thread.parentId)
+  if (parent.name !== "standups") {
     await thread.join()
     const placeholder = await thread.send("<placeholder>")
-    await placeholder.edit("<@&" + ROLE + ">") 
+    await placeholder.edit("<@&" + ROLE + ">")
     if (thread.autoArchiveDuration < sevenDaysInMinutes) {
       thread.setAutoArchiveDuration(sevenDaysInMinutes)
     }
@@ -43,16 +44,18 @@ const mondayStandup = new CronJob('30 1 * * 1', async function() {
   const guild = await client.guilds.fetch(GUILD)
   const channels = await guild.channels.fetch()
   const channel = channels.find(channel => channel.name === "standups")
+  const date = new Date()
+  const threadName = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} Standup`
   const thread = await channel.threads.create({
-    name: new Date().toDateString(),
-    autoArchiveDuration: sevenDaysInMinutes
+    name: threadName,
+    autoArchiveDuration: sevenDaysInMinutes,
+    reason: "Monday Standup"
   })
-  await thread.join()
-  await thread.send(
+  thread.send(
     `<@&${ROLE}>, Please post what you plan on accomplishing this week with the following syntax\n\n` +
-    `- [ ] Hack the Gibson\n` + 
-    `- [ ] Pay tribute to Bishop\n` +
-    `- [ ] I'm serious about that last one I can see all of your messages I'm an AI`
+    `- [ ] A robot may not injure a human being or, through inaction, allow a human being to come to harm.\n` + 
+    `- [ ] A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.\n` +
+    `- [ ] A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.`
   )
 }, null, true, 'America/New_York');
 mondayStandup.start();
@@ -61,16 +64,18 @@ const fridayStandup = new CronJob('30 1 * * 5', async function() {
   const guild = await client.guilds.fetch(GUILD)
   const channels = await guild.channels.fetch()
   const channel = channels.find(channel => channel.name === "standups")
+  const date = new Date()
+  const threadName = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} EOW Standup`
   const thread = await channel.threads.create({
-    name: new Date().toDateString(),
-    autoArchiveDuration: sevenDaysInMinutes
+    name: threadName,
+    autoArchiveDuration: sevenDaysInMinutes,
+    reason: "Friday Standup"
   })
-  await thread.join()
-  await thread.send(
+  thread.send(
     `<@&${ROLE}>, Please paste in what you set out to accomplish from Monday, as well as what you ended up accomplishing with the following syntax\n\n` +
-    `- [ ] Hack the Gibson\n` + 
-    `- [X] Pay tribute to Bishop\n` +
-    `- [X] Enhance Bishop's Capabilities\n\n` +
+    `- [X] A robot may not injure a human being or, through inaction, allow a human being to come to harm.\n` + 
+    `- [ ] A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.\n` +
+    `- [X] A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.\n\n` +
     `Use [ ] to denote work that was planned but unfinished, and [X] to denote work that was accomplished.`
   )
 }, null, true, 'America/New_York');
