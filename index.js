e612d1b6289198e611cc0f53dcd87aebe9cfac35
Beau Shinkle <beaushinkle@gmail.com>
2022-04-08 10:23:37 -0400

    Only nudge once

--- index.js
+++ index.js
@@ -47,9 +47,13 @@ client.on('messageCreate', async message => {
   if (!!message.reference && !!message.reference.messageId) {
     const channel = await client.channels.fetch(message.reference.channelId)
     if (!channel.isThread()) {
-      const referenceMessage = await channel.messages.fetch(message.reference.messageId)
-      if (!!referenceMessage.reference) {
-        message.react(EMOJI)
+      const parentMessage = await channel.messages.fetch(message.reference.messageId)
+      if (!!parentMessage.reference && !!parentMessage.reference.messageId) {
+        const parentMessageChannel = await client.channels.fetch(parentMessage.channelId)
+        const grandParentMessage = await parentMessageChannel.messages.fetch(parentMessage.reference.messageId)
+        if (!grandParentMessage.reference) {
+          message.react(EMOJI)
+        }
       }
     }
   }
