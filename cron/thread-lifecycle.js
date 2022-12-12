d33f13ed15436fe78e87251a30ced4051cac19a2
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 15:30:00 -0500

    Factor out the thread lifecycle cron job

---
+++ cron/thread-lifecycle.js
@@ -0,0 +1,73 @@
+const GUILD = process.env.GUILD
+
+const { MessageActionRow, MessageButton } = require('discord.js')
+const moment = require('moment')
+
+const { read, write } = require('../storage')
+
+function weekdaysBefore(theMoment, days) {
+  let newMoment = theMoment.clone()
+  while(days > 0) {
+    if (newMoment.isoWeekday() < 6) {
+      days -= 1
+    }
+    newMoment = newMoment.subtract(1, 'days')
+  }
+  return newMoment
+}
+
+module.exports = {
+  // schedule: '*/15 * * * *',
+  schedule: '* * * * *',
+  timezone: 'America/New_York',
+  execute(client) {
+    return async () => {
+      const guild = await client.guilds.fetch(GUILD)
+      const channels = await guild.channels.fetch()
+      const longRunningThreadIds = (await read('long-running-thread-ids')) || {}
+      // const archiveThreshold = weekdaysBefore(moment(), 4)
+      const archiveThreshold = moment()
+      channels
+        .filter(channel => channel.isText() && channel.name != "keep-github" && channel.viewable)
+        .forEach(async channel => {
+          const threads = await channel.threads.fetch()
+          threads.threads.forEach(async thread => {
+            const messages = await thread.messages.fetch({limit: 1})
+            const lastActivity = Math.max(
+              messages.first() && messages.first().createdTimestamp || 0,
+              thread.archiveTimestamp
+            )
+            if (moment(lastActivity).isBefore(archiveThreshold)) {
+              if (longRunningThreadIds[thread.id]) {
+                await thread.setArchived(true)
+                thread.setArchived(false)
+              } else {
+                if (thread.ownerId === client.user.id) {
+                  thread.setArchived(true)
+                } else {
+                  const row = new MessageActionRow()
+                    .addComponents(
+                      new MessageButton()
+                      .setCustomId('archive-thread')
+                      .setLabel('Archive The Thread')
+                      .setStyle('DANGER'),
+                    )
+                    .addComponents(
+                      new MessageButton()
+                      .setCustomId('long-running-thread')
+                      .setLabel('Long-Running Thread')
+                      .setStyle('SECONDARY'),
+                    )
+
+                  thread.send({
+                    content: `<@${thread.ownerId}>, it's been a bit since this thread has seen activity. Ready to archive it?`,
+                    components: [row]
+                  })
+                }
+              }
+            }
+          })
+        })
+    }
+  }
+}
