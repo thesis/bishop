32b1de871c497520e9872364d24b3ea60932645f
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 17:40:06 -0500

    De-nest create-thread-reminder

--- scripts/create-new-thread-reminder.js
+++ scripts/create-new-thread-reminder.js
@@ -4,18 +4,24 @@ module.exports = {
   trigger: 'messageCreate',
   execute(client) {
     return async (message) => {
-      if (!!message.reference && !!message.reference.messageId) {
-        const channel = await client.channels.fetch(message.reference.channelId)
-        if (!channel.isThread()) {
-          const parentMessage = await channel.messages.fetch(message.reference.messageId)
-          if (!!parentMessage.reference && !!parentMessage.reference.messageId) {
-            const parentMessageChannel = await client.channels.fetch(parentMessage.channelId)
-            const grandParentMessage = await parentMessageChannel.messages.fetch(parentMessage.reference.messageId)
-            if (!grandParentMessage.reference) {
-              message.react(EMOJI)
-            }
-          }
-        }
+      if (!message.reference || !message.reference.messageId) {
+        return
+      }
+
+      const channel = await client.channels.fetch(message.reference.channelId)
+      if (channel.isThread()) {
+        return
+      }
+
+      const parentMessage = await channel.messages.fetch(message.reference.messageId)
+      if (!parentMessage.reference || !parentMessage.reference.messageId) {
+        return
+      }
+
+      const parentMessageChannel = await client.channels.fetch(parentMessage.channelId)
+      const grandParentMessage = await parentMessageChannel.messages.fetch(parentMessage.reference.messageId)
+      if (!grandParentMessage.reference) {
+        message.react(EMOJI)
       }
     }
   },
