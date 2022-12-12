a18164fbdc11f9ba0aa1299e90f3187ebd9db05b
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 17:46:47 -0500

    De-nest track-missing-standups

--- scripts/track-missing-standups.js
+++ scripts/track-missing-standups.js
@@ -5,16 +5,20 @@ module.exports = {
   trigger: 'messageCreate',
   execute(client) {
     return async (message) => {
-      if (message.author.id !== client.user.id) {
-        const channel = await client.channels.fetch(message.channelId)
-        if (channel.isThread() && channel.name.endsWith("Standup")) {
-          const messages = await channel.messages.fetch()
-          const reminderMessage = messages.find(message => message.author.id === client.user.id && message.content.startsWith("I would appreciate"))
-          if (reminderMessage) {
-            const membersToRemind = missingStandups(client, channel).map(user => user.username)
-            reminderMessage.edit(`I would appreciate standup posts from:\n\n${membersToRemind.join('\n')}`)
-          }
-        }
+      if (message.author.id === client.user.id) {
+        return
+      }
+
+      const channel = await client.channels.fetch(message.channelId)
+      if (!channel.isThread() || !channel.name.endsWith("Standup")) {
+        return
+      }
+
+      const messages = await channel.messages.fetch()
+      const reminderMessage = messages.find(message => message.author.id === client.user.id && message.content.startsWith("I would appreciate"))
+      if (reminderMessage) {
+        const membersToRemind = missingStandups(client, channel).map(user => user.username)
+        reminderMessage.edit(`I would appreciate standup posts from:\n\n${membersToRemind.join('\n')}`)
       }
     }
   },
