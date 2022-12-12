4f524938eb3eceee062aed3fa920ca566c45c78c
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 17:41:46 -0500

    De-nest manage-thread

--- scripts/manage-thread.js
+++ scripts/manage-thread.js
@@ -6,13 +6,15 @@ module.exports = {
   trigger: 'threadCreate',
   execute(client) {
     return async (thread) => {
-      if (thread.ownerId !== client.user.id) {
-        await thread.join()
-        const placeholder = await thread.send("<placeholder>")
-        await placeholder.edit("<@&" + ROLE + ">")
-        if (thread.autoArchiveDuration < sevenDaysInMinutes) {
-          thread.setAutoArchiveDuration(sevenDaysInMinutes)
-        }
+      if (thread.ownerId === client.user.id) {
+        return
+      }
+
+      await thread.join()
+      const placeholder = await thread.send("<placeholder>")
+      await placeholder.edit("<@&" + ROLE + ">")
+      if (thread.autoArchiveDuration < sevenDaysInMinutes) {
+        thread.setAutoArchiveDuration(sevenDaysInMinutes)
       }
     }
   },
