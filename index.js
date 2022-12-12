19a4ad9c9df97828ba13c801a8c093136e06626a
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 15:52:53 -0500

    Factor out the command executor

--- index.js
+++ index.js
@@ -37,19 +37,4 @@ for (const file of cronFiles) {
   job.start()
 }
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return
-
-	const command = client.commands.get(interaction.commandName)
-
-	if (!command) return
-
-	try {
-		await command.execute(interaction, client)
-	} catch (error) {
-		console.error(error)
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-	}
-})
-
 client.login(TOKEN)
