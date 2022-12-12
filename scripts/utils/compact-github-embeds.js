dce7cfc7f770628ac742099c2370657cc67b4809
Beau Shinkle <beaushinkle@gmail.com>
2022-12-12 17:34:43 -0500

    Remove a layer of nesting from compact-embeds

--- scripts/utils/compact-github-embeds.js
+++ scripts/utils/compact-github-embeds.js
@@ -4,17 +4,19 @@ module.exports = {
   async compactGithubEmbeds(message) {
     if (!message.author.bot) {
       const receivedEmbeds = message.embeds
-      if (!!receivedEmbeds && receivedEmbeds.find(embed => embed.url && embed.url.includes('github'))) {
-        await message.suppressEmbeds(true)
-        const description = receivedEmbeds
-          .map((embed, i) => `(${i+1}) [${embed.title}](${embed.url})`)
-          .join('\n')
-        const embed = new MessageEmbed()
-          .setColor('#0099ff')
-          .setDescription(description)
-
-        message.channel.send({embeds: [embed]})
+      if (!receivedEmbeds || !receivedEmbeds.find(embed => embed.url && embed.url.includes('github'))) {
+        return
       }
+
+      await message.suppressEmbeds(true)
+      const description = receivedEmbeds
+        .map((embed, i) => `(${i+1}) [${embed.title}](${embed.url})`)
+        .join('\n')
+      const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setDescription(description)
+
+      message.channel.send({embeds: [embed]})
     }
   }
 }
